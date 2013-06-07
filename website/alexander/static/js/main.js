07206c8db1a2781bbe4b4cb4424349bd46ab027a
Aaron Ogle <atogle@gmail.com>
2013-06-07 16:18:29 -0400

    Support Feed item POST and DELETE

--- website/alexander/static/js/main.js
+++ website/alexander/static/js/main.js
@@ -1,22 +1,41 @@
+/*globals Backbone $ Handlebars _ */
+
 var Alexander = Alexander || {};
 
 (function(NS) {
-
   Backbone.Marionette.TemplateCache.prototype.compileTemplate = function(rawTemplate) {
     return Handlebars.compile(rawTemplate);
   };
 
+  NS.App = new Backbone.Marionette.Application();
+
+  NS.App.addRegions({
+    content: '#content'
+  });
+
+  NS.App.on('initialize:after', function(){
+    Backbone.history.start();
+  });
+
   NS.FeedModel = Backbone.Model.extend({});
 
   NS.FeedCollection = Backbone.Collection.extend({
-    url: '/api/feeds',
+    url: '/api/feeds/',
     model: NS.FeedModel
   });
 
   NS.FeedView = Backbone.Marionette.ItemView.extend({
     template: "#feed-item-tpl",
     tagName: 'li',
-    className: 'feed-item well'
+    className: 'feed-item well',
+    events: {
+      'click .delete-feed-link': 'delete'
+    },
+    delete: function() {
+      if (window.confirm('Are you sure you want to delete this feed?')) {
+        this.model.destroy();
+      }
+    }
   });
 
   NS.FeedListView = Backbone.Marionette.CollectionView.extend({
@@ -29,6 +48,19 @@ var Alexander = Alexander || {};
           el: '#feeds-list',
           collection: collection
         });
+
+    $('#feed-form').on('submit', function(evt) {
+      evt.preventDefault();
+      var formArray = $(this).serializeArray(),
+          modelObj = {};
+
+      _.each(formArray, function(obj){
+        modelObj[obj.name] = obj.value;
+      });
+
+      collection.create(modelObj);
+      this.reset();
+    });
 
     collection.fetch();
   });
