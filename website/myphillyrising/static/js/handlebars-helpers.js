85ed3cade8eca28010ad1f85b13a44fe232b74d6
Andy Cochran <acochran@openplans.org>
2013-06-13 14:01:18 -0400

    style events list with .calendar-icon

--- website/myphillyrising/static/js/handlebars-helpers.js
+++ website/myphillyrising/static/js/handlebars-helpers.js
@@ -7,3 +7,19 @@ Handlebars.registerHelper('fromnow', function(datetimeAttr) {
 Handlebars.registerHelper('prettydate', function(datetimeAttr) {
   return moment(this[datetimeAttr]).format('MMMM Do YYYY h:mma');
 });
+
+Handlebars.registerHelper('prettymonth', function(datetimeAttr) {
+  return moment(this[datetimeAttr]).format('MMM');
+});
+
+Handlebars.registerHelper('prettyday', function(datetimeAttr) {
+  return moment(this[datetimeAttr]).format('D');
+});
+
+Handlebars.registerHelper('prettyyear', function(datetimeAttr) {
+  return moment(this[datetimeAttr]).format('YYYY');
+});
+
+Handlebars.registerHelper('prettytime', function(datetimeAttr) {
+  return moment(this[datetimeAttr]).format('h:mma');
+});
