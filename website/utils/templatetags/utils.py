583ecfd5d56cc6988453daae1b569c506b3ce0e2
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-08-01 16:33:44 -0400

    Use the JSONEncoder from rest_framework to encode json data in the templates

--- website/utils/templatetags/utils.py
+++ website/utils/templatetags/utils.py
@@ -1,10 +1,11 @@
 import json
 from django.template import Library
 from django.utils.safestring import mark_safe
+from rest_framework.utils.encoders import JSONEncoder
 
 register = Library()
 
 
 @register.filter
 def as_json(data):
-    return mark_safe(json.dumps(data))
+    return mark_safe(json.dumps(data, cls=JSONEncoder))
