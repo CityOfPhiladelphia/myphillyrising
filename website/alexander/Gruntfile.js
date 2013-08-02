db88501ed344dc6ae78ea36b10b6a997e14306e4
Aaron Ogle <atogle@gmail.com>
2013-08-01 21:45:09 -0400

    Add grunt to merge and minify client assets for alexander - Add package.json and Gruntfile - Add debug condition in the template as to use min files or not

---
+++ website/alexander/Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      dist: {
+        files: [
+          {
+            dest: 'static/js/admin-components.min.js',
+            src: [
+              'static/components/jquery/jquery.min.js',
+              'static/components/underscore/underscore-min.js',
+              'static/components/backbone/backbone-min.js',
+              'static/components/backbone.marionette/lib/backbone.marionette.js',
+              'static/components/handlebars.js/dist/handlebars.js',
+              'static/components/moment/moment.js',
+              'static/components/select2/select2.js'
+            ]
+          },
+          {
+            dest: 'static/js/admin-app.min.js',
+            src: [
+              'static/js/django-csrf.js',
+              '../myphillyrising/static/js/handlebars-helpers.js',
+              'static/js/models.js',
+              'static/js/admin-views.js',
+              'static/js/utils.js'
+            ]
+          },
+          {
+            dest: 'static/js/modernizr.min.js',
+            src: [
+              'static/components/modernizr/modernizr.js'
+            ]
+          }
+        ]
+      }
+    },
+    cssmin: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      dist: {
+        files: [
+          {
+            dest: 'static/css/admin-components.min.css',
+            src: [
+              'static/components/bootstrap/docs/assets/css/bootstrap.css',
+              'static/components/select2/select2.css',
+            ]
+          }
+        ]
+      }
+    }
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'cssmin']);
+
+};
