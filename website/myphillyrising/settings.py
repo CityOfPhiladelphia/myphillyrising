5fc0eb4fda57d7b289ec779acf93ab51821620f8
Aaron Ogle <atogle@gmail.com>
2013-08-02 10:54:11 -0400

    Set ALLOWED_HOSTS

--- website/myphillyrising/settings.py
+++ website/myphillyrising/settings.py
@@ -24,7 +24,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 ###############################################################################
 #
