df978e891ea16026364249885fa508f3a50e4257
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-08-09 16:04:45 -0400

    Add middleware to handle social_auth exceptions

--- website/myphillyrising/settings.py
+++ website/myphillyrising/settings.py
@@ -99,6 +99,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'social_auth.middleware.SocialAuthExceptionMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
