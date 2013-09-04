3fc20cc6dac2abc69e1b8e9b836f12e554ae91d0
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-09-04 13:27:45 -0400

    Inform the user when their public stuff login fails

--- website/publicstuff_tools/social_auth/urls.py
+++ website/publicstuff_tools/social_auth/urls.py
@@ -5,4 +5,7 @@ urlpatterns = patterns(
     url(r'^login/publicstuff/proxy$',
         'publicstuff_tools.social_auth.views.publicstuff_auth_view',
         name='publicstuff-auth'),
+    url(r'^login/publicstuff/invalid$',
+        'publicstuff_tools.social_auth.views.publicstuff_invalid_login_view',
+        name='publicstuff-invalid-login'),
 )
--- website/publicstuff_tools/social_auth/views.py
+++ website/publicstuff_tools/social_auth/views.py
@@ -1,7 +1,9 @@
 import json
 from urllib import urlencode
 from urllib2 import urlopen
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView
 
 
 def publicstuff_auth_view(request):
@@ -12,12 +14,21 @@ def publicstuff_auth_view(request):
     publicstuff_url = ('https://www.publicstuff.com/api/2.0/user_login/?' +
                        urlencode(credentials))
     callback_url = request.REQUEST.get('callback')
+    invalid_url = reverse('publicstuff-invalid-login')
 
     publicstuff = urlopen(publicstuff_url)
     data = json.load(publicstuff)
 
     if isinstance(data, dict) and 'response' in data:
         data = data['response']
+
+    status = data.get('status', {})
+    if status.get('code') != 200:
+        return HttpResponseRedirect(invalid_url)
+
     request.session['publicstuff_auth_data'] = data
 
     return HttpResponseRedirect(callback_url)
+
+
+publicstuff_invalid_login_view = TemplateView.as_view(template_name='publicstuff_tools/invalid_login.html')
