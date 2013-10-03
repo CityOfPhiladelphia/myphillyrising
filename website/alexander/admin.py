83444605eb911296f75c19d04cc92b0578808cfb
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-10-03 00:37:01 -0400

    Improve the Django admin page for content items - Add search field for titles - Add filtering on tags and feeds

--- website/alexander/admin.py
+++ website/alexander/admin.py
@@ -4,7 +4,8 @@ from .models import Feed, ContentItem, ContentTag
 
 class ContentItemAdmin (admin.ModelAdmin):
     list_display = ('__unicode__', 'displayed_from', 'category', 'feed')
-    list_filter = ('category',)
+    list_filter = ('category', 'tags', 'feed')
+    search_fields = ('title',)
 
 
 admin.site.register(Feed)
